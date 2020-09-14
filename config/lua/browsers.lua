90e6fe60b9db3c6b5c78b486aa2a5d9e3cf49b13
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2020-09-14 18:59:28 +0430

    fix: naxi base rules

--- config/lua/browsers.lua
+++ config/lua/browsers.lua
@@ -5,21 +5,23 @@ local firefox_ios_latest = 28
 local chrome_latest = 85
 
 function _M.is_modern(agent_name, agent_os, agent_version_major)
-  if agent_name == 'chrome' and agent_version_major >= chrome_latest then
-    return '1'
-  end
-  if agent_name == 'firefox' then
-    if (agent_os == 'iphone' or agent_os == 'ipad') and agent_version_major >= firefox_ios_latest then
+  if agent_name and agent_version_major and agent_version_major then
+    if agent_name == 'chrome' and agent_version_major >= chrome_latest then
       return '1'
-    elseif agent_version_major >= firefox_latest then
-      return '1'
+    end
+    if agent_name == 'firefox' then
+      if (agent_os == 'iphone' or agent_os == 'ipad') and agent_version_major >= firefox_ios_latest then
+        return '1'
+      elseif agent_version_major and agent_version_major >= firefox_latest then
+        return '1'
+      end
     end
   end
   return '0'
 end
 
 function _M.is_modern_or_crawler(category, agent_name, agent_version_major)
-  if category == 'crawler' then
+  if category and category == 'crawler' then
     return '1'
   end
   return _M.is_modern(agent_name, agent_version_major)
